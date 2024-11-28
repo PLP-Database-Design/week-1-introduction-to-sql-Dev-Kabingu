a5bd16a5b3e2b66692b87693e8bf65da9b2234c5
dev-kabingu <missmercykabingu@gmail.com>
2024-11-28 20:32:45 +0300

    assignment one

---
+++ answer.sql
@@ -0,0 +1,50 @@
+-- question one
+DBMS is a software that facilitates the storage, mangement, retrival of data in an organized manner.
+components: database schema which defines the organization of data.
+            query processor which intreprates and executes database queries.
+            database engine which allows the access and data management.
+            data management language that allow the manipulation of data.
+            security manager which controls users access to data.
+            backup and recovery manager which protects data by providing regular updates and recovery mechanisms
+
+-- question 2
+Relational database is a database that store data in tables which are interelated.
+example: mysql, postgresql, microsoft SQL server, oracle database.
+
+-- question 3
+data defination language which is used to define the strcture of the database.
+data manipulation language which is used to manipulate data with operations like to retrieve, update, insert, delete 
+data control language which is used to give privillages to users through the grant and revoke command.
+
+
+-- question 4
+A primary key is a filed that is used to identify a record uniquely while a foreign key is a key that is a field in a table that 
+links the table to a primary key in another table.
+
+-- question 5
+ERD is a graphical representation of the realtionship among entities in a database.
+
+
+-- question 6
+It is easy to query 
+Flexible as they can handle complex queries.
+Reduces data duplication through normalization
+Robust security feature through access control to prevent unauthorized access.
+data integrity by using the primary and foreign keys which ensures accuracy and consistence.
+
+
+-- question 7
+interger which stores whole numbers
+varchar which stores variable length strings ot text.
+data and time.
+float and decimals
+
+
+-- question 8
+ data organization
+ data security
+ data integrity
+ data recovery
+ concurrency control
+
+         
